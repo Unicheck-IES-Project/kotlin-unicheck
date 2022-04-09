46e236bbe5a19856fd9918fbf0c317195d682c25
Nicolas De Maio <nicolas.demaio19@gmail.com>
2022-04-09 20:51:03 -0300

    Initial commit

---
+++ src/main/kotlin/com/unicheck/kotlinunicheckbackend/KotlinUnicheckBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.unicheck.kotlinunicheckbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinUnicheckBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinUnicheckBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/unicheck/kotlinunicheckbackend/KotlinUnicheckBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.unicheck.kotlinunicheckbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinUnicheckBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
